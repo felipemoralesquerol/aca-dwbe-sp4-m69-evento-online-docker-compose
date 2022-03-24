2fc166806d0af2a961973e7e2c9588acb700d0ce
Felipe Morales <felipe.morales.querol@gmail.com>
2022-03-23 21:36:44 -0300

    Fix general

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2021: true
+
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: [2, 'always'],
+    'comma-dangle': ['error', 'never']
+
+  }
+};
